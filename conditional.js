806ef6c6ebc4b425f1bc44e833b52b5114dba601
Aroarafat <arafathossainbcpsc@gmail.com>
2021-08-01 20:42:05 +0600

    second-commit

---
+++ conditional.js
@@ -0,0 +1,8 @@
+var murgirrun = 200;
+var amarkasease = 150;
+if (murgirrun > amarkasease) {
+    console.log('run khabo r haddi chibabo');
+}
+else {
+    console.log('run pamuna haddio chibamuna');
+}
---
+++ multiple-condition.js
@@ -0,0 +1,14 @@
+var myjob = true;
+var mymoney = 20000;
+if (myjob == true && mymoney > 2000) {
+    console.log('biya hobe');
+}
+else {
+    console.log('biya hobena');
+}
+if (myjob == true || mymoney > 200000) {
+    console.log('biya hobe');
+}
+else {
+    console.log('biya hobena');
+}
